3da588c1b324f4c32cc5e77e343df4a3dc29d3c6
Андрей Галеев <galeev.kamensk33@mail.ru>
2020-06-24 05:48:28 +0500

    Добавьте файлы проекта.

---
+++ ООП 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП_2
+{
+    class Program
+    {
+        class TV
+        {
+            public int currentСhannel = 0;
+        }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Переключение канала стрелочками");
+
+                TV Channel = new TV();
+                bool tv = true;
+            int currentChannel = 0; 
+
+            while (tv)
+                {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        {
+                            currentChannel--;
+                            Console.WriteLine("Текущий канал: " + currentChannel);
+                            break;
+                        }
+                    case ConsoleKey.RightArrow:
+                        {
+                            currentChannel++;
+                            Console.WriteLine("Текущий канал: " + currentChannel);
+                            break;
+                        }
+
+                }
+
+                Console.WriteLine("Выбран канал " + currentChannel);
+
+            }
+                Console.ReadLine();
+            
+        }
+    }
+}
